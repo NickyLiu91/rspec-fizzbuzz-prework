34925b2b9af0af51374e6722ace1887cb9eb668b
Nicky Liu <nickyliu91@gmail.com>
2018-08-10 03:55:26 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(number)
-  if number % 3 == 0
-    return "Fizz"
-  elsif number % 5 == 0
-    return "Buzz"
-  else number % 3 && number % 5
+  if number % 3 && number % 5
     return "FizzBuzz"
+  elsif number % 3 == 0
+    return "Fizz"
+  else number % 5
+    return "Buzz"
   end
 end
